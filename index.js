a3a2220efd1cc0ce130e8d50e4d53ffe234e96b4
bishnu-47 <bd23122000@gmail.com>
2021-03-30 21:03:41 +0530

    included pokedex api

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express")
+const bodyParser = require("body-parser")
 const exphbs = require("express-handlebars")
+const Pokedex = require('pokedex-promise-v2');
+
 const app = express()
+const P = new Pokedex();
 
 // express-handlebars setup
 app.engine('hbs', exphbs({
@@ -9,9 +13,23 @@ app.engine('hbs', exphbs({
 }));
 
 app.set('view engine', 'hbs');
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.get("/", (req,res)=>{
 	res.render("home")
+})
+
+app.post("/submit", async (req,res)=>{
+	const searchedPokemon = req.body.pokename.trim()
+	try{
+		const pokeData = await P.getPokemonByName(searchedPokemon)
+		console.log(pokeData)
+		res.render("pokemon",{
+			data: pokeData
+		})
+	}catch(e){
+		console.log(e)
+	}
 })
 
 const PORT = 3000
